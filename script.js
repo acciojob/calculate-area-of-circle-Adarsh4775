67ae059a927d817d2ca5fb5e35d7ecfba58fb475
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-10-31 19:53:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 	  if(radius > 0)
 	  {
-         let radiusofCircle = (3.14*(radius**2)).toFixed(2);
+         let radiusofCircle = 3.14*(radius**2).toFixed(2);
 		  return "The area of the circle with radius " + radius + " is " + radiusofCircle; 
 	  }
 	 else{  
@@ -10,9 +10,9 @@
 	 }
     }
  let radius = parseFloat(prompt("Enter the radius of the circle:"));
-calculateArea(radius);    
+ 
 
 alert(calculateArea(radius));  
     
-  
+calculateArea(radius);   
 
